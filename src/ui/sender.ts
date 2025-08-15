2bef9ba5a268a5526bdbc597fc2d81ad71c2a867
rosbeldev <jorgerosbelcruzarias@gmail.com>
2025-08-15 14:34:18 +0200

    ✨ feat: add StatsCards component to display data cards

--- src/ui/sender.ts
+++ src/ui/sender.ts
@@ -2,10 +2,12 @@ import { SEOHead } from './SEOHead';
 import { ResponsiveHeader } from './ResponsiveHeader';
 import { SimpleHero } from './SimpleHero';
 import { DualColumnHero } from './DualColumnHero';
+import { StatsCards } from './StatsCards';
 
 export const components = {
   SEOHead: SEOHead,
   ResponsiveHeader: ResponsiveHeader,
   SimpleHero: SimpleHero,
   DualColumnHero: DualColumnHero,
+  StatsCards: StatsCards,
 };
